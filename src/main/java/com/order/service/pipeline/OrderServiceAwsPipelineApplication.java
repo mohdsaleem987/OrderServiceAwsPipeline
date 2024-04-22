7d1f8a247190e1494e8ee1af55ed779dd8b08f5f
mohdsaleem987 <mohdsaleem987@gmail.com>
2024-04-22 12:28:40 +0530

    First Commit

---
+++ src/main/java/com/order/service/pipeline/OrderServiceAwsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.order.service.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceAwsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceAwsPipelineApplication.class, args);
+    }
+
+}
